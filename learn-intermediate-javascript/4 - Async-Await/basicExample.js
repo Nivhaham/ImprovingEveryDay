f76411526481e8e282cc4be97ef32a057214dd2c
Niv <daganniv@gmail.com>
2024-01-06 23:04:02 +0200

    Better

---
+++ learn-intermediate-javascript/4 - Async-Await/basicExample.js
@@ -0,0 +1,18 @@
+/*Async Await are Syntatics Sugar of Promise presented in ES8*/
+
+const cookBeanSouffle = require('./library.js');
+
+
+async function hostDinnerParty(){
+  try{
+    let dinner = await cookBeanSouffle();
+     console.log(`${dinner} is served!`);
+  }
+  catch(error) {
+   console.log(error);
+    console.log('Ordering a pizza!');
+  }
+}
+hostDinnerParty()
+
+
